296037aa38e607a5a114ea2c799a10d7a618eb1f
Abhighna0704 <abhighnasashok@gmail.com>
2025-04-09 12:43:06 +0530

    Update score_calculator.py

--- score_calculator.py
+++ score_calculator.py
@@ -1,1 +1,16 @@
+import unittest
+from score_calculator import calculate_average_score
+
+class TestScoreCalculator(unittest.TestCase):
+    def test_average_score(self):
+        feedback = [
+            {"score": 3},
+            {"score": 5},
+            {"score": 4}
+        ]
+        avg = calculate_average_score(feedback)
+        self.assertEqual(avg, 4)
+
+if __name__ == "__main__":
+    unittest.main()
 
