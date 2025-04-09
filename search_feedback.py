6ac0bea98abfee5a88b7bfe2930dd439b8947ac3
Abhighna0704 <abhighnasashok@gmail.com>
2025-04-09 12:43:34 +0530

    Update search_feedback.py

--- search_feedback.py
+++ search_feedback.py
@@ -1,1 +1,7 @@
+def count_feedback_by_course(feedback_list):
+    count = {}
+    for fb in feedback_list:
+        course = fb["course_code"]
+        count[course] = count.get(course, 0) + 1
+    return count
 
