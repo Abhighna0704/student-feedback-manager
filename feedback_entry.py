d11608e133eb3d8734dc9e9dfd1426e2bedddf1f
Abhighna0704 <abhighnasashok@gmail.com>
2025-04-09 12:40:53 +0530

    Update feedback_entry.py

--- feedback_entry.py
+++ feedback_entry.py
@@ -1,1 +1,9 @@
+def add_feedback(student_id, course_code, feedback_text, score):
+    feedback = {
+        "student_id": student_id,
+        "course_code": course_code,
+        "feedback_text": feedback_text,
+        "score": score
+    }
+    return feedback
 
